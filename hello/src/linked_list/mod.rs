1b735fbe959c1ac80ebd26cc0d3dd0a2a9a544cc
stream2000 <stream2000@139.com>
2020-07-23 23:04:52 +0800

    second

--- hello/src/linked_list/mod.rs
+++ hello/src/linked_list/mod.rs
@@ -1,3 +1,4 @@
+mod array;
 pub mod second;
 pub mod first;
 
--- hello/src/linked_list/second.rs
+++ hello/src/linked_list/second.rs
@@ -1,5 +1,6 @@
 use std::mem;
 
+// leaning rust with too many linkedList
 pub struct List<T> {
     head: Link<T>,
 }
@@ -12,7 +13,9 @@ struct Node<T> {
     next: Link<T>,
 }
 
+// list is a container
 impl<T> List<T> {
+    // we don't need to explicit declare it to be 'static'.
     pub fn new() -> Self {
         List { head: None }
     }
@@ -26,10 +29,25 @@ impl<T> List<T> {
         self.head = Some(new_node);
     }
 
+    // replace with empty and take
+    // just don't let it dangling
     pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        // what is as_ref
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
         })
     }
 }
@@ -43,17 +61,61 @@ impl<T> Drop for List<T> {
     }
 }
 
+// generic tuple
+pub struct IntoIter<T>(List<T>);
 
 
+impl<T> List<T> {
+    // a constructor of iter
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+// this is the simplest iter.
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
+
+// iter -> next
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        // map from box to the ref of box
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    // so type itself is also a variable
+    // why do we need use
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
+            &node.elem
+        })
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::List;
+    use std::borrow::BorrowMut;
 
     #[test]
     fn basics() {
         let mut list = List::new();
 
+        let a = [1, 1];
         // Check empty list behaves right
         assert_eq!(list.pop(), None);
 
@@ -77,5 +139,53 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| {
+            *value = 42
+        });
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
